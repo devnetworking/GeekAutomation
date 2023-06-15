f636962917b14fdd8196c211bc9a0e9543dfcaf2
Hamza <hamzamouadden@yahoo.ca>
2023-06-14 20:31:22 -0400

    dev

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
-const app = express();
+const express = require('express');
 const routes = require('./config/routes');
+const bodyParser = require('body-parser');
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+
+require('./utils/passport')(passport);
+
+const app = express();
+
+app.use(session({
+    secret: 'secret', 
+    resave: true,
+    saveUninitialized: true
+  }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, '/app/views'));
 //app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(routes);
 
 app.listen(3000, () => console.log('Server started on port 3000'));
--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -2,11 +2,15 @@ const User = require('../models/User');
 const Task = require('../models/Task');
 
 exports.getIndex = async (req, res, next) => {
-  try {
-    const tasks = await Task.findAll();
-    res.render('home/index', { tasks: tasks });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
+  if (req.isAuthenticated()) {
+    try {
+      const tasks = await Task.findAll();
+      res.render('home/index', { tasks: tasks });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  } else {
+    res.redirect('/login');
   }
 };
